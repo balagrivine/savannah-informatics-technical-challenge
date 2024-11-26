3a07bb3e4214438de23b4a69bad6d244bb80fb40
balagrivine <balagrivine@gmail.com>
2024-11-26 02:44:46 -0800

    chore(ci): add testing to the cicd pipeline

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,14 +1,14 @@
 -- This script creates sql tables in the database
 
+CREATE DATABASE IF NOT EXISTS savannah_ecommerce
 
 -- Create a customers table if it does not exist in the db
 CREATE TABLE IF NOT EXISTS customers (
 	id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
+	name VARCHAR(50) NOT NULL,
 	email VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(200),
-	shipping_address VARCHAR(100) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
 	created_at DATE DEFAULT NOW()
 );
 
@@ -24,18 +24,10 @@ CREATE TABLE IF NOT EXISTS products (
 -- Create an orders table if it does not exist in the db
 CREATE TABLE IF NOT EXISTS orders (
 	id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	order_date DATE NOT NULL,
-	price DECIMAL(10, 2) NOT NULL,
-	customer_id INTEGER,
-	FOREIGN KEY (customer_id) REFERENCES customers (id)
-);
-
--- Create an order_details tale if it does not exist in the db
-CREATE TABLE IF NOT EXISTS order_details (
-	id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	order_id INTEGER NOT NULL,
+	order_date TIMESTAMP NOT NULL DETAULT CURRENT_TIMESTAMP,
+	total_amount DECIMAL(10, 2) NOT NULL,
+	customer_id INTEGER NOT NULL,
 	product_id INTEGER NOT NULL,
-	quantity INTEGER DEFAULT 0,
-	FOREIGN KEY (order_id) REFERENCES orders (id),
-	FOREIGN KEY (product_id) REFERENCES products (id)
+	FOREIGN KEY (product_id) REFERENCES products (id),
+	FOREIGN KEY (customer_id) REFERENCES customers (id)
 );
